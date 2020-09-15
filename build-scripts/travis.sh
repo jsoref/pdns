d3aa9a4f87f4b27aea5bb28faf9d42722bdbfccc
Josh Soref <jsoref@users.noreply.github.com>
2020-09-14 23:07:18 -0400

    give up?

--- build-scripts/travis.sh
+++ build-scripts/travis.sh
@@ -239,7 +239,6 @@ install_auth() {
   run "sudo systemctl restart slapd"
   run "popd"
   run "sudo -u openldap mkdir -p /var/lib/ldap/powerdns"
-  run "sudo ldapadd -Y EXTERNAL -H ldapi:/// -f ./build-scripts/docker/ldap-regress/bootstrap/ldif/powerdns-mdb.ldif"
   run "sudo systemctl stop slapd"
 
   # remote-backend tests requirements
